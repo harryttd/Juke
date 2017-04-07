f2612aa5a722918ff1529f603185b48361a26028
Aryeh Harris <harryttd@gmail.com>
2017-04-07 17:47:16 -0400

    Removed db-init and postinstall scripts from the package.json as they were preventing heroku from deploying. Put the scripts in comments in the server/env/index.js file.

--- server/env/index.js
+++ server/env/index.js
@@ -5,10 +5,10 @@ const devConfigPath = path.join(__dirname, './development.js');
 const productionConfigPath = path.join(__dirname, './production.js');
 
 if (process.env.NODE_ENV === 'production') {
+  // THESE TWO LINES IN THE PACKAGE JSON WERE PREVENTING HEROKU FROM DEPLOYING
+  // "db-init": "pg-init juke",
+  // "postinstall": "npm run db-init"
   module.exports = require(productionConfigPath);
 } else {
   module.exports = require(devConfigPath);
 }
-
-// "db-init": "pg-init juke",
-// "postinstall": "npm run db-init"
