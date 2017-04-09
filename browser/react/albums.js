d4d98700e99aa076af883f1ad295dbc79a1d292d
Aryeh Harris <harryttd@gmail.com>
2017-04-09 16:54:26 -0400

    Add start watch script.

--- browser/react/albums.js
+++ browser/react/albums.js
@@ -2,7 +2,6 @@
 import React from 'react';
 
 export default ({ albums, clickHandler }) => (
-
   <div>
     <h3>Albums</h3>
     <div className="row">
@@ -10,7 +9,7 @@ export default ({ albums, clickHandler }) => (
         albums.map(album => {
           return (
             <div key={ album.id } className="col-xs-4">
-              <a className="thumbnail" href="#" onClick={ () => clickHandler(album.id) }>
+              <a className="thumbnail" onClick={ () => clickHandler(album.id) }>
                 <img src={ album.imageUrl } />
                 <div className="caption">
                   <h5>
@@ -26,34 +25,3 @@ export default ({ albums, clickHandler }) => (
     </div>
   </div>
 );
-
-// export default class Albums extends React.Component {
-//
-//   render() {
-//     const props = this.props;
-//     const albumElements = props.albums.map(album => {
-//       return (
-//         <div key={ album.id } className="col-xs-4">
-//           <a className="thumbnail" href="#" onClick={ () => props.clickHandler(album.id) }>
-//             <img src={ album.imageUrl } />
-//             <div className="caption">
-//               <h5>
-//                 <span>{ album.name }</span>
-//               </h5>
-//               <small>{ album.songs.length } songs</small>
-//             </div>
-//           </a>
-//         </div>
-//       );
-//     });
-//
-//     return (
-//       <div>
-//         <h3>Albums</h3>
-//         <div className="row">
-//           { albumElements }
-//         </div>
-//       </div>
-//     );
-//   }
-// }
